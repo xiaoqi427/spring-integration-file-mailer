b778c238b5b53b16a7159a4a12c2d2f19d3fe4a5
James Tayler <kanjiforbreakfast@live.com>
2016-02-08 09:33:27 +0000

    Spring Initializr project start

---
+++ src/main/java/io/tayler/SpringIntegrationFileMailerApplication.java
@@ -0,0 +1,12 @@
+package io.tayler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationFileMailerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationFileMailerApplication.class, args);
+	}
+}
---
+++ src/test/java/io/tayler/SpringIntegrationFileMailerApplicationTests.java
@@ -0,0 +1,16 @@
+package io.tayler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringIntegrationFileMailerApplication.class)
+public class SpringIntegrationFileMailerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
