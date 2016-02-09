aec707188b598771b91d347be31eb7c5dce9033c
James Tayler <kanjiforbreakfast@live.com>
2016-02-09 20:00:53 +0000

    refactored fileInputChannel for readability and added mail outbound channel

--- src/main/java/io/tayler/SpringIntegrationFileMailerApplication.java
+++ src/main/java/io/tayler/SpringIntegrationFileMailerApplication.java
@@ -8,10 +8,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.core.Pollers;
+import org.springframework.integration.dsl.file.FileInboundChannelAdapterSpec;
 import org.springframework.integration.dsl.file.Files;
 import org.springframework.integration.dsl.mail.Mail;
 import org.springframework.integration.dsl.support.Transformers;
 import org.springframework.integration.file.filters.LastModifiedFileListFilter;
+import org.springframework.integration.file.transformer.FileToByteArrayTransformer;
 
 @SpringBootApplication
 public class SpringIntegrationFileMailerApplication {
@@ -25,16 +27,45 @@ public class SpringIntegrationFileMailerApplication {
 	}
 	
 	@Bean
-	public IntegrationFlow inboundFiles() {
-		return IntegrationFlows.from(Files.inboundAdapter(new File("/home/vagrant/loan-applications"))
-				.preventDuplicates().ignoreHidden().regexFilter("*.pdf").filter(minuteOldFile()),
-				e -> e.poller(Pollers.fixedDelay(10000)).id("inboundFileChannel"))
-				.transform(Transformers.fileToByteArray())
-				.channel("emailFileChannel")
+	public IntegrationFlow prepareInboundFilesToBeEmailedFlow() {
+		return IntegrationFlows.from(inboundFileChannel(), e -> e.poller(Pollers.fixedDelay(10000)))
+				.transform(intoEmailAttachment())
+				.channel("emailFileAsAttachmentOutboundChannel")
 				.get();
 	}
 	
-	private LastModifiedFileListFilter minuteOldFile() {
+	@Bean
+	public IntegrationFlow emailFileAsAttachmentFlow() {
+	    return IntegrationFlows.from("emailFileAsAttachmentOutboundChannel")
+	            .handle(Mail.outboundAdapter("smtp.sendgrid.net")
+	                            .port(587)
+	                            .credentials("user", "pw")
+	                            .protocol("smtp")
+	                            .javaMailProperties(p -> p.put("mail.debug", "true")),
+	                    e -> e.id("sendMailEndpoint"))
+	            .get();
+	}
+
+	
+	
+	
+	@Bean
+	public FileInboundChannelAdapterSpec inboundFileChannel() {
+		return Files.inboundAdapter(new File("/home/vagrant/loan-applications"))
+				.preventDuplicates()
+				.ignoreHidden()
+				.regexFilter("*.pdf")
+				.filter(giveFilesTimeToFinishWritingToDisk());
+	}
+	
+	
+	@Bean
+	public FileToByteArrayTransformer intoEmailAttachment() {
+		return Transformers.fileToByteArray();
+	}
+	
+	@Bean
+	public LastModifiedFileListFilter giveFilesTimeToFinishWritingToDisk() {
 		LastModifiedFileListFilter filter = new LastModifiedFileListFilter();
 		filter.setAge(60000);
 		return filter; 
