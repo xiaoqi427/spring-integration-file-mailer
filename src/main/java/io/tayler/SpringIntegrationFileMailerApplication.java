5b3b3424be4183c8be163f85912721ef146af693
James Tayler <kanjiforbreakfast@live.com>
2016-02-10 13:17:10 +0000

    implemented the outbound mail flow. The adapter is now in a working state, as long as everything is configured correctly!

--- src/main/java/io/tayler/SpringIntegrationFileMailerApplication.java
+++ src/main/java/io/tayler/SpringIntegrationFileMailerApplication.java
@@ -8,10 +8,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.core.Pollers;
-import org.springframework.integration.dsl.file.FileInboundChannelAdapterSpec;
 import org.springframework.integration.dsl.file.Files;
 import org.springframework.integration.dsl.mail.Mail;
 import org.springframework.integration.dsl.support.Transformers;
+import org.springframework.integration.file.FileReadingMessageSource;
 import org.springframework.integration.file.filters.LastModifiedFileListFilter;
 import org.springframework.integration.file.transformer.FileToByteArrayTransformer;
 
@@ -19,25 +19,29 @@ import org.springframework.integration.file.transformer.FileToByteArrayTransform
 public class SpringIntegrationFileMailerApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(SpringIntegrationFileMailerApplication.class, args);
-
 		new SpringApplicationBuilder(SpringIntegrationFileMailerApplication.class)
         	.web(false)
         	.run(args);
 	}
-	
+
 	@Bean
-	public IntegrationFlow prepareInboundFilesToBeEmailedFlow() {
+	public IntegrationFlow start_prepareInboundFilesToBeEmailedFlow() {
 		return IntegrationFlows.from(inboundFileChannel(), e -> e.poller(Pollers.fixedDelay(10000)))
-				.transform(intoEmailAttachment())
+				.transform(fileIntoEmailAttachment())
 				.channel("emailFileAsAttachmentOutboundChannel")
 				.get();
 	}
-	
+
 	@Bean
-	public IntegrationFlow emailFileAsAttachmentFlow() {
+	public IntegrationFlow end_emailFileAsAttachmentFlow() {
 	    return IntegrationFlows.from("emailFileAsAttachmentOutboundChannel")
-	            .handle(Mail.outboundAdapter("smtp.sendgrid.net")
+	    		.enrichHeaders(Mail.headers()
+	    				.attachmentFilename("filename.pdf")
+	    				.to("example@example.com")
+	    				.from("example@example.com")
+	    				.subject("here is your file")
+	    		)
+	            .handle(Mail.outboundAdapter("localhost")
 	                            .port(587)
 	                            .credentials("user", "pw")
 	                            .protocol("smtp")
@@ -46,28 +50,26 @@ public class SpringIntegrationFileMailerApplication {
 	            .get();
 	}
 
-	
-	
-	
+
 	@Bean
-	public FileInboundChannelAdapterSpec inboundFileChannel() {
-		return Files.inboundAdapter(new File("/home/vagrant/loan-applications"))
+	public FileReadingMessageSource inboundFileChannel() {
+		return Files.inboundAdapter(new File("/inbound-file-directory-here"))
 				.preventDuplicates()
 				.ignoreHidden()
-				.regexFilter("*.pdf")
-				.filter(giveFilesTimeToFinishWritingToDisk());
+				.regexFilter(".*\\.pdf")
+				.filter(giveFilesTimeToFinishWritingToDisk())
+				.get();
 	}
-	
-	
+
 	@Bean
-	public FileToByteArrayTransformer intoEmailAttachment() {
+	public FileToByteArrayTransformer fileIntoEmailAttachment() {
 		return Transformers.fileToByteArray();
 	}
-	
+
 	@Bean
 	public LastModifiedFileListFilter giveFilesTimeToFinishWritingToDisk() {
 		LastModifiedFileListFilter filter = new LastModifiedFileListFilter();
 		filter.setAge(60000);
-		return filter; 
+		return filter;
 	}
 }
